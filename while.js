09c8e63498f514848e159fd38200f90d3d662d31
mostafizur-rahman <dev.mostafizurrahman@gmail.com>
2024-09-28 20:42:34 +0600

    more practice about while loop

---
+++ while.js
@@ -0,0 +1,22 @@
+// const number = 10 ;
+// if(number < 11){
+//     console.log("the number is so small")
+
+// }
+// console.log("try to a big number")
+
+// let num = 0 ;
+
+// while(num < 5){
+//     console.log("loopping", num);
+//     num = num + 1 ;
+
+// }
+
+let number = 0 ;
+
+while(number < 20 ){
+    console.log(" I love you tasnim")
+    number++
+
+}
