cef09a7be618d8ef88035b0daad1e263742ae0ba
mostafizur-rahman <dev.mostafizurrahman@gmail.com>
2024-09-28 22:05:18 +0600

    show increment and decrement number by using for loops

---
+++ decremental.js
@@ -0,0 +1,10 @@
+// increment number
+for(let i = 0 ; i <=10 ; i++){
+    console.log("show increment numbers 0 to 100 :", i)
+}
+
+// decrement number
+
+for(let i = 10 ; i >=0 ; i--){
+    console.log("show decrement number of 0 to 100 :", i)
+}
