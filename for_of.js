6216c325d729d74e9b9b43fe1a438d8d06ff6ca0
mostafizur-rahman <dev.mostafizurrahman@gmail.com>
2024-09-28 19:53:20 +0600

    added more practice

--- for_of.js
+++ for_of.js
@@ -1,7 +1,23 @@
 const friends = ["Monarul", "Rizu", "Ashraful", "Johurul", "Forhad", "Shanto"];
+console.log(friends)
 
 for(const friend of friends){
     
     console.log(friend)
-    console.log("they are my best friends")
+    
+}
+
+
+const numbers = [12,14,16,43,33,100];
+console.log(numbers)
+
+for(const number of numbers){
+    console.log(number)
+}
+
+const fruits = ["apple", "banana", "jackfruits", "watermelon", "guava"];
+console.log(fruits)
+
+for(const fruit of fruits){
+    console.log(fruit)
 }
