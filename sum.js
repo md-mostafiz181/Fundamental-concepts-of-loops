4d7521662dc88ca6e70f4af177c03c0aea7944fa
mostafizur-rahman <dev.mostafizurrahman@gmail.com>
2024-09-28 21:01:26 +0600

    show even number from a loop

---
+++ sum.js
@@ -0,0 +1,10 @@
+let num = 0 ;
+let sum = 0 ;
+
+while(num <= 10 ){
+    // console.log("show the total number :", num);
+    sum = sum + num;
+    console.log("show the added number :", sum)
+    num++ ;
+
+}
