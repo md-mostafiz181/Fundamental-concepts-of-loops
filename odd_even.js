ca1267448ccbfc923f19093793105556ee1c2454
mostafizur-rahman <dev.mostafizurrahman@gmail.com>
2024-09-28 21:20:48 +0600

    show add and even number by using for loop

---
+++ odd_even.js
@@ -0,0 +1,10 @@
+//show even number
+for(let i = 0; i <=10 ; i=i+2){
+    console.log("show even number :", i)
+}
+
+// show odd number
+
+for(let i = 1 ; i <= 10 ; i=i+2){
+    console.log("show odd number :", i)
+}
