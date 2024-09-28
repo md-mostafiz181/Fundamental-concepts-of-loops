f39f0b543189ef8a2bc3384e2ba0b47741f10be8
mostafizur-rahman <dev.mostafizurrahman@gmail.com>
2024-09-28 19:45:31 +0600

    starting fundamantal concepts of loops

---
+++ README.md
@@ -0,0 +1,1 @@
+"# Fundamental-concepts-of-loops" 
---
+++ for_of.js
@@ -0,0 +1,7 @@
+const friends = ["Monarul", "Rizu", "Ashraful", "Johurul", "Forhad", "Shanto"];
+
+for(const friend of friends){
+    
+    console.log(friend)
+    console.log("they are my best friends")
+}
